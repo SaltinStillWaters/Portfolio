46868465e362d4beebde4f97a4dd38bfbc3dfe0e
SaltinStillWaters <SaltinStillWaters@gmail.com>
2026-03-08 13:39:22 +0800

    init commit

---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <About />
+  <Projects @onClick="projectClick"/>
+  <ProjectOverlay :open-overlay="openOverlay", :project-id="projectId" @closed="openOverlay=false"/>
+  <Experience />
+  <Resume />
+  <Contact />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+  const projectId = ref(0);
+  const openOverlay = ref(false);
+
+  function projectClick(i) {
+    openOverlay.value = true;
+    projectId.value = i
+  }
+</script>
